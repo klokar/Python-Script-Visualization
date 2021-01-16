45fc2287f592f1d07a5504a5df8c2edf5eef15d8
Klemen Lokar <klemen.lokar@hotmail.com>
2021-01-16 10:15:58 +0100

    translations

--- resources/views/processor/list.blade.php
+++ resources/views/processor/list.blade.php
@@ -40,8 +40,8 @@
     </div>
     <div class="pb-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-5 sm:p-6">
-                <div class="py-4">Seznam dependencijev</div>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-6 pb-6">
+                <div class="px-6 py-4 rounded-b-md text-xs text-gray uppercase bg-gray-200 tracking-widest w-max mb-6">Seznam dependencijev</div>
                 @if(empty($dependencies))
                     <div class="container">Dependenciji niso naloženi ali pa je napaka v datoteki!</div>
                 @else
